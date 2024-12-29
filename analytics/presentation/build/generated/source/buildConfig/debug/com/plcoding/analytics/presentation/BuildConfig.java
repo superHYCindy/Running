17a67bbaeb3e36eb85ed72c1db2764c3539d6181
superheeyoung <tjdgmldud12@gmail.com>
2024-12-29 17:42:52 +0900

    run setting

---
+++ analytics/presentation/build/generated/source/buildConfig/debug/com/plcoding/analytics/presentation/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.plcoding.analytics.presentation;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.plcoding.analytics.presentation";
+  public static final String BUILD_TYPE = "debug";
+  // Field from build type: debug
+  public static final String API_KEY = "nBPxeXR0YYXjxPtw";
+  // Field from build type: debug
+  public static final String BASE_URL = "https://runique.pl-coding.com:8080";
+}
